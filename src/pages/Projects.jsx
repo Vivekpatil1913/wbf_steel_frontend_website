c88343e1e193d83e7440b0cf9d35050dbeef3aa2
DhanuPatil0654 <dhananjaypatil572001@gmail.com>
2025-11-14 18:51:25 +0530

    project pg1 and project pg - 2 are completed

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import '../css/project.css'
 
 import com_img from '../../public/assets/project img/Commercial_img.png'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 
 import brand1 from '../../public/assets/project img/brand1.png'
 import brand2 from '../../public/assets/project img/brand2.png'
@@ -23,28 +22,64 @@ import product8 from '../../public/assets/project img/project_page2/product8.png
 import product9 from '../../public/assets/project img/project_page2/product9.png'
 import product10 from '../../public/assets/project img/project_page2/product10.png'
 
+import Card from 'react-bootstrap/Card';
+
+import project_banner from '../../public/assets/project img/project_banner.png'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import Slider from "react-slick";
-const images = [
-  com_img,
-  com_img,
-  com_img,
-  com_img,
-  com_img,
-  com_img,
-];
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { Link } from 'react-router-dom'
+
+
 
 function Projects() {
-  const settings = {
-    dots: true,
+
+  const sliderRefs = useRef([]);   // store refs for each project
+
+
+  const MAX_LENGTH = 287;
+
+  const paradata = [
+    {
+      text:
+        "Located in Georgia, USA, WBF Steel Company specializes in commercial building projects. The company provides high-quality structural steel fabrication, design, and erection services for industrial and commercial constructions. Known for durability, precision, and timely project delivery"
+    }
+  ];
+
+  const isLong = paradata[0].text.length > MAX_LENGTH;
+
+  const truncatedText = isLong
+    ? paradata[0].text.substring(0, MAX_LENGTH)
+    : paradata[0].text;
+
+
+
+  const sliderSettings = {
+    dots: false,
     infinite: true,
-    vertical: true,
-    verticalSwiping: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
+
+    responsive: [
+      {
+        breakpoint: 576, // MOBILE VIEW
+        settings: {
+          vertical: false,           // horizontal sliding
+          verticalSwiping: false,
+          slidesToShow: 2,           // show 2 images at a time
+          slidesToScroll: 1,
+          swipeToSlide: true,
+          centerMode: false,
+        }
+      }
+    ]
   };
+
 
   const productdata = [
     {
@@ -89,107 +124,195 @@ function Projects() {
     },
 
   ]
+
+  const images = [
+    product1, product2, product3, product4, product5, product7
+  ];
+
+  const projects = [
+    {
+      name: "Project 1",
+      location: "Commercial Building, GA USA",
+      description: "Located in Georgia, USA, WBF Steel Company specializes in commercial building projects. The company provides high-quality structural steel fabrication, design, and erection services for industrial and commercial constructions. Known for durability, precision, and timely project delivery",
+      mainImage: product1,
+
+      sliderImages: [product1, product2, product3, product3]
+    },
+    {
+      name: "Project 2",
+      location: "Commercial Test, GA USA",
+      description: "Located in Georgia, USA, WBF Steel Company specializes in commercial building projects. The company provides high-quality structural steel fabrication, design, and erection services for industrial and commercial constructions. Known for durability, precision, and timely project delivery ",
+      mainImage: product10,
+
+      sliderImages: [product10, product5, product8, product9, product7]
+    },
+
+
+
+  ];
+
+
+  const [showProductSection, setShowProductSection] = useState(false);
+
+
+
   return (
     <>
-
+      <img src={project_banner} class="img-fluid" alt="project_img" width={'100%'}/>
       <h1 className='text-center head_project mt-3 mb-3'>Built to Power Your Product</h1>
       <div className='container'>
-        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-          <li class="nav-item" role="presentation ">
-            <button class="nav-link active" id="project1-tab" data-bs-toggle="pill" data-bs-target="#project1" type="button" role="tab" aria-controls="project1" aria-selected="true">Project 1</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="project2-tab" data-bs-toggle="pill" data-bs-target="#project2" type="button" role="tab" aria-controls="project2" aria-selected="false">Project 2</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="project3-tab" data-bs-toggle="pill" data-bs-target="#project3" type="button" role="tab" aria-controls="project3" aria-selected="false">Project 3</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="project4-tab" data-bs-toggle="pill" data-bs-target="#project4" type="button" role="tab" aria-controls="project4" aria-selected="false">Project 4</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="project5-tab" data-bs-toggle="pill" data-bs-target="#project5" type="button" role="tab" aria-controls="project5" aria-selected="false">Project 5</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="project6-tab" data-bs-toggle="pill" data-bs-target="#project6" type="button" role="tab" aria-controls="project6" aria-selected="false">Project 6</button>
-          </li>
-        </ul>
-        <div class="tab-content" id="pills-tabContent">
-          <div class="tab-pane fade show active " id="project1" role="tabpanel" aria-labelledby="project1-tab">
-            {/* <div class="card mt-5 mb-5">
-              <div class="card-body">
-                <div class="container-fluid">
-                  <div class="row">
-                    <div class="col-lg-3 col-md-4 col-12">
+        <div className="tabs-wrapper">
+          <ul
+            className={`nav nav-pills mb-3 tab-scroll ${projects.length === 1 ? "single-tab" : ""
+              }`}
+            id="pills-tab"
+            role="tablist"
+          >
+            {projects.map((project, index) => (
+              <li className="nav-item" role="presentation" key={index}>
+                <button
+                  className={`nav-link w-100 text-center ${index === 0 ? "active" : ""
+                    }`}
+                  id={`project${index}-tab`}
+                  data-bs-toggle="pill"
+                  data-bs-target={`#project${index}`}
+                  type="button"
+                  role="tab"
+                  aria-controls={`project${index}`}
+                  aria-selected={index === 0}
+                >
+                  {project.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-                      One of three columns
-                    </div>
-                    <div class="col-lg-5 col-md-4 col-12">
-                      One of three columns
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-12">
-                      One of three columns
-                    </div>
-                  </div>
+
+
+
+
+
+
+        {!showProductSection && (
+          <>
+            <div className="tab-content" id="pills-tabContent">
+              {projects.map((project, index) => (
+                <div
+                  className={`tab-pane fade ${index === 0 ? "show active" : ""}`}
+                  id={`project${index}`}
+                  role="tabpanel"
+                  aria-labelledby={`project${index}-tab`}
+                  key={index}
+                >
+                  <Card className='main_card'>
+                    <Card.Body>
+                      <div className="row card-row">
+
+                        {/* Column 1 - Slider */}
+                        <div className="col-lg-3 col-md-3 col-sm-12 slider-col mt-3 mb-3">
+                          <button
+                            className="custom-btn slider-btn top-btn"
+                            onClick={() => sliderRefs.current[index]?.slickPrev()}
+                          >
+                            <i class="fa-solid fa-arrow-up"></i>
+                          </button>
+
+                          <div className="slider-wrapper">
+                            <Slider
+                              ref={(el) => (sliderRefs.current[index] = el)}
+                              {...sliderSettings}
+                            >
+                              {project.sliderImages.map((img, i) => (
+                                <div key={i} className="slider-item">
+                                  <img src={img} className="img-fluid slide-img" alt="slide" />
+                                </div>
+                              ))}
+                            </Slider>
+                          </div>
+
+                          <button
+                            className="custom-btn slider-btn bottom-btn"
+                            onClick={() => sliderRefs.current[index]?.slickNext()}
+                          >
+                            <i class="fa-solid fa-arrow-down"></i>
+                          </button>
+                        </div>
+
+                        {/* Column 2 */}
+                        <div className="col-lg-4 col-md-4 col-sm-12 p-0 m-0">
+                          <p className="Commercial_name">{project.location}</p>
+                          <img src={project.mainImage} height="320" width="100%" className="product_img" />
+                        </div>
+
+                        {/* Column 3 */}
+                        <div className="col-lg-5 col-md-5 col-sm-12 text-box">
+                          <div className="para_background">
+                            <p>
+                              {project.description.length > MAX_LENGTH
+                                ? project.description.substring(0, MAX_LENGTH)
+                                : project.description}
+
+                              {project.description.length > MAX_LENGTH && (
+                                <FaExternalLinkAlt
+                                  className="external-icon"
+                                  onClick={() => setShowProductSection(true)}
+                                  style={{ cursor: "pointer" }}
+                                />
+                              )}
+                            </p>
+                          </div>
+                        </div>
+
+                      </div>
+                    </Card.Body>
+                  </Card>
                 </div>
-              </div>
-            </div> */}
+              ))}
+            </div>
 
-            <div class="card mt-3 mb-3 border-top-dashed-custom" >
+            {/* Bottom WBF Section */}
+            <div class="card mt-5 mb-3 border-top-dashed-custom mb-5">
               <div class="card-body">
                 <div class="container">
                   <div class="row">
                     <div class="col-lg-5 col-md-5 col-sm-12">
                       <p className='work_proces_text'>We work across the <b>world</b></p>
                       <p className='conactus_name mt-3'>Connect with Us</p>
-                      <p className='para_conatc'>Contact WBF Steel for professional advice on steel detailing and industry best
-                        Practices</p>
+                      <p className='para_conatc'>Contact WBF Steel for professional advice on steel detailing</p>
                       <p className='get_in_touch mt-5'>Get in Touch <i class="fa-solid fa-up-right-from-square ml-3"></i></p>
                     </div>
-                    <div class="col-lg-7 col-md-7 col-sm-12">
-                      <div class="card our_experties mt-4 mb-4">
-                        <div class="card-body">
-                          <p className='para_experites'>Our Expertise Lies In</p>
-                          <div className='row'>
-                            <div className='col-lg-6 col-md-6 col-sm-6'>
-                              <img src={brand1} className="img-fluid mx-auto d-block" />
-                            </div>
-                            <div className='col-lg-6 col-md-6 col-sm-6'>
-                              <img src={brand2} className="img-fluid mx-auto d-block" />
-                            </div>
-                            <div className='col-lg-6 col-md-6 col-sm-6'>
-                              <img src={brand3} className="img-fluid mx-auto d-block" />
-                            </div>
-                            <div className='col-lg-6 col-md-6 col-sm-6'>
-                              <img src={brand4} className="img-fluid mx-auto d-block" />
-                            </div>
 
-                            <img src={brand5} className="img-fluid" alt="brand" style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '60%', height: '60%' }} />
+                    <div class="col-lg-7 col-md-7 col-sm-12 experties-card-wrapper">
+                      <Card className="experties-card">
+                        <Card.Body>
+                          <p className='experties text-center'>Our Expertise Lies In</p>
 
-
+                          <div className="brands-wrapper">
+                            {[brand1, brand2, brand3, brand4, brand5].map((img, i) => (
+                              <img key={i} src={img} className="brand-img" alt="brand" />
+                            ))}
                           </div>
-                        </div>
-                      </div>
-
+                        </Card.Body>
+                      </Card>
                     </div>
-
                   </div>
                 </div>
               </div>
             </div>
+          </>
+        )}
 
-          </div>
-          <div class="tab-pane fade" id="project2" role="tabpanel" aria-labelledby="project2-tab">
 
+        {showProductSection && (
+          <>
             <div className="row justify-content-center">
               {productdata.length === 1 ? (
-                // If only ONE product, center it
                 <div className="col-lg-4 col-md-6 col-sm-12 text-center">
                   <img src={productdata[0].product_img} className="img-fluid img_product" />
                   <p className="product_name mt-3">{productdata[0].text}</p>
                 </div>
               ) : (
-                // Otherwise, show all normally
                 productdata.map((a, index) => (
                   <div key={index} className="col-lg-4 col-md-6 col-sm-12 text-center">
                     <div className="img_container">
@@ -212,22 +335,14 @@ function Projects() {
                     </div>
                     <div className="col-lg-6 col-md-6 col-sm-12 colou2">
                       <p className='workingpara'>We work across the <b className='bold_work'>world</b></p>
-                      <p className='para_card mt-3'>Contact WBF Steel for professional advice on steel detailing and industry best
-                        Practices</p>
+                      <p className='para_card mt-3'>Contact WBF Steel for professional advice on steel detailing</p>
                     </div>
                   </div>
-
-
                 </div>
               </div>
             </div>
-
-          </div>
-          <div class="tab-pane fade" id="project3" role="tabpanel" aria-labelledby="project3-tab">Project 3</div>
-          <div class="tab-pane fade" id="project4" role="tabpanel" aria-labelledby="project4-tab">Project 4</div>
-          <div class="tab-pane fade" id="project5" role="tabpanel" aria-labelledby="project5-tab">Project 5</div>
-          <div class="tab-pane fade" id="project6" role="tabpanel" aria-labelledby="project6-tab">Project 6</div>
-        </div>
+          </>
+        )}
 
       </div>
     </>
@@ -235,4 +350,5 @@ function Projects() {
 }
 
 export default Projects
+
 
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,6 +6,8 @@ import About from '../pages/About'
 import Services from "../pages/Services";
 import Projects from "../pages/Projects";
 import ContactUs from "../pages/ContactUs";
+import ProjectDetails from "../pages/ProjectDetails";
+import ProjectNav from "../components/projectCom/ProjectNav";
 
 function AppRoutes() {
   return (
@@ -16,7 +18,9 @@ function AppRoutes() {
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
+        {/* <Route path="/projects" element={<ProjectNav />} /> */}
         <Route path="/projects" element={<Projects />} />
+        {/* <Route path="/projects/details" element={<ProjectDetails />} /> */}
         <Route path="/contactus" element={<ContactUs />} />
       </Routes>
       <Footer />
